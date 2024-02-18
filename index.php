5e7d13f2a619178a036151aaebd3f22df139de4c
Hugo Felippe de Souza Cruz <hfelipecruz07@gmail.com>
2024-02-18 18:25:29 -0300

    fix: composer index

--- index.php
+++ index.php
@@ -5,8 +5,22 @@ header('Cache-Control: no-cache');
 header('Connection: keep-alive');
 header('X-Accel-Buffering: no');
 
-$events = 10;
-while ($events >= 0) {
+$statusCode = 200;
+// Emulate sending headers like a PHP framework would.
+header('Connection: keep-alive', false, $statusCode);
+header('Content-Type: text/event-stream', true, $statusCode);
+header('Cache-Control: no-cache, private, max-age=0', false, $statusCode);
+header('X-Accel-Buffering: no', false, $statusCode); // Disables FastCGI Buffering on Nginx.
+header('HTTP/1.1 200 OK', true, $statusCode);
+
+if (ob_get_length()) {
+    ob_end_clean();
+}
+
+flush();
+
+$events = 100;
+while ($events > 0) {
     echo "event: ping\n";
     $curDate = date(DATE_ISO8601);
     echo 'data: {"time": "' . $curDate . '"}';
