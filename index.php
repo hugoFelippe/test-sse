0693b7fc69d045f56a8205aae4cdce35345564f1
Hugo Felippe de Souza Cruz <hfelipecruz07@gmail.com>
2024-02-18 18:18:53 -0300

    feat: ajustes

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
 
-use Cowsayphp\Farm;
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+header('Connection: keep-alive');
+header('X-Accel-Buffering: no');
 
-header('Content-Type: text/plain');
+$events = 10;
+while ($events >= 0) {
+    echo "event: ping\n";
+    $curDate = date(DATE_ISO8601);
+    echo 'data: {"time": "' . $curDate . '"}';
+    echo "\n\n";
 
-$text = "Set a message by adding ?message=<message here> to the URL";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
+    if (ob_get_length()) {
+        ob_end_clean();
+    }
+
+    flush();
+
+    // Interrompe o loop se o cliente abortou a conexão (fechou a página)
+    if (connection_aborted()) {
+        break;
+    };
+
+    sleep(1);
+    $events--;
 }
-
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
